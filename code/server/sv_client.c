1c9fee065ebbed912ed8de8f8fac13a31cc7492e
Mr <Ye@h>
2019-12-07 23:44:38 +0000

    changed forceGear logic

--- code/server/sv_client.c
+++ code/server/sv_client.c
@@ -1340,6 +1340,10 @@ void SV_UpdateUserinfo_f( client_t *cl ) {
 
 	Q_strncpyz( cl->userinfo, Cmd_Argv(1), sizeof(cl->userinfo) );
 
+	if (sv_forceGear && Q_stricmp(sv_forceGear->string, "null")) {
+		Info_SetValueForKey(cl->userinfo, "gear", sv_forceGear->string);
+	}
+
 	SV_UserinfoChanged( cl );
 	// call prog code to allow overrides
 	VM_Call( gvm, GAME_CLIENT_USERINFO_CHANGED, cl - svs.clients );
